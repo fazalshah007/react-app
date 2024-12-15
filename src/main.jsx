1c8c1dc965d183c1fb9b2e8cedc5d29e1a7ba149
fazalshah007 <bilalfazal52@gmail.com>
2024-12-15 17:07:52 +0500

    create project in react using vite

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello Jawan Pakistan</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis tempora cupiditate amet molestias dolores labore deserunt, harum dolorum laborum veniam, aspernatur esse ipsa quos tempore ipsum? Odit, qui soluta? Obcaecati eum qui cupiditate voluptas asperiores vitae tempore unde quas laudantium suscipit harum odit perferendis provident adipisci ab impedit delectus, accusamus, quos, et quam aliquam alias. Amet repellat voluptatum, aperiam, quas ipsa exercitationem autem nobis voluptates alias totam soluta ex nemo quibusdam doloremque maxime dolorem consequuntur libero laudantium? Nam odit vel excepturi iusto quae? Doloribus perferendis hic dolor sint molestiae accusantium excepturi repudiandae quisquam deserunt iure earum minima fugiat quaerat, voluptate mollitia quas velit saepe, quod culpa voluptates maxime laudantium expedita. Consequatur alias, quam temporibus distinctio ad dolor labore adipisci nobis quos ipsum. Esse earum provident labore et ipsum rem sit reprehenderit! Iusto unde quasi, blanditiis voluptates eligendi, tenetur nam odio voluptas, excepturi error delectus expedita porro saepe illum. Laudantium perferendis earum sint, nostrum dolorem molestiae. Quibusdam totam, accusamus excepturi veniam quos nesciunt animi dolorem, veritatis beatae consequatur blanditiis rem officiis alias dolores modi cupiditate dicta voluptates doloribus cum fugit, quam quidem voluptatem voluptatum laborum! Omnis praesentium quas corporis ad, qui labore quibusdam sed eligendi aut neque delectus facere cumque.</p>
+    <img width={400} src="https://img.freepik.com/free-photo/sports-car-races-through-dark-blurred-motion-generative-ai_188544-12490.jpg" alt="" />
+  </div>
+)
